4da0df7cfb23bc8b7610fe5f07b45a1b044696c6
Tania Bilqis Thoharoh <82069706+TaniaBilqisThoharoh@users.noreply.github.com>
2021-11-06 18:58:10 +0700

    modul05

---
+++ src/com/taniabilqist/modul5/Percobaan/Classroom.java
@@ -0,0 +1,28 @@
+package com.taniabilqist.modul5.Percobaan;
+
+public class Classroom {
+
+    private int classcode;
+    private String classname;
+    private Student student;
+
+    public Classroom(int classcode, String classname){
+        this.classcode = classcode;
+        this.classname = classname;
+    }
+    public void setStudent(Student student){
+        this.student = student;
+    }
+
+    public int getClasscode() {
+        return classcode;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public String getStudentName() {
+        return student.getNama();
+    }
+}
