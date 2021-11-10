cde4bf1042bf7ab85adc1e54237c21100cad6360
Tania Bilqis Thoharoh <82069706+TaniaBilqisThoharoh@users.noreply.github.com>
2021-11-10 21:33:20 +0700

    modul06

---
+++ src/com/taniabilqist/modul6/Percobaan/Animal.java
@@ -0,0 +1,24 @@
+package com.taniabilqist.modul6.Percobaan;
+
+public class Animal {
+
+    public String name = "unnamed Animal";
+    public int age = 0;
+
+    public Animal(){
+
+    }
+    public Animal(String name){
+        this.name = name;
+    }
+
+    public Animal (String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void walk(){
+        System.out.println(this.name + "walks!");
+        System.out.println();
+    }
+}
