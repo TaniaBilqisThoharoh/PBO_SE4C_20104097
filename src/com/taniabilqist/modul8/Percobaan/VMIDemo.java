701c7bcfc32a6493353f6237fb209c5601ff7379
Tania Bilqis Thoharoh <82069706+TaniaBilqisThoharoh@users.noreply.github.com>
2021-12-12 22:40:41 +0700

    modul08

---
+++ src/com/taniabilqist/modul8/Percobaan/VMIDemo.java
@@ -0,0 +1,13 @@
+package com.taniabilqist.modul8.Percobaan;
+
+public class VMIDemo {
+
+    public static void main(String[] args) {
+        Salary s = new Salary("Hero", "kubar", 3, 5000);
+        s.checkMail();
+
+        Employee e = new Salary("Hardin", "jabar", 4, 2500);
+        e.checkMail();
+    }
+
+    }
